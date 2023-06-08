8e74dae7400378fa75c848e27947cdb1fb9feeb6
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2023-06-09 05:47:35 +0900

    remove _reset.scss, change dist filename

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 // gulpfile.js
 
 const gulp = require('gulp')
-const sass = require('gulp-dart-sass')
 const plumber = require('gulp-plumber')
 const gulpDartSass = require('gulp-dart-sass')
 const sassGlob = require('gulp-sass-glob-use-forward')
@@ -9,33 +8,8 @@ const autoprefixer = require('gulp-autoprefixer')
 const gcmq = require('gulp-group-css-media-queries')
 const sourcemaps = require('gulp-sourcemaps')
 
-// ----------------------------------------------------------------------
-
-// モジュール読み込み
-//----------------------------------------------------------------------
-/* gulp */
-// const gulp = require('gulp')
-// const { src, dest, watch, series, parallel } = require('gulp')
-
-// /* sass */
-
-// exports.sass = sass
-
-// EOF
-
-// const sass = require('gulp-sass')(require('sass'))
-// const watch = require('gulp-watch')
 const cssmin = require('gulp-cssmin')
-const rename = require('gulp-rename')
-// const sourcemaps = require('gulp-sourcemaps')
 const concat = require('gulp-concat')
-
-// // Task: watch
-// gulp.task('watch', function () {
-//     watch(['sass/**/*scss'], function () {
-//         gulp.src('sass/**/*scss').pipe(sass()).pipe(gulp.dest('./css'))
-//     })
-// })
 
 gulp.task('build', function (done) {
     gulp.src('./dist/**/*.css', { base: './' })
